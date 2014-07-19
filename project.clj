8f0fdac86dec5856d86bbb9aec5df549bb657336
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2014-07-19 22:22:52 +0300

    minor build tweaks

--- project.clj
+++ project.clj
@@ -5,21 +5,33 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2202"]
-                 [net.drib/strokes "0.5.1"]
-                 [om "0.6.2"]
+                 [om "0.6.4"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :source-paths ["src"]
-  :cljsbuild {:builds [{:source-paths ["src"]
+  :cljsbuild {:builds {:dev
+                       {:source-paths ["src"]
                         :compiler {:output-to "out/main.js"
                                    :output-dir "out/"
                                    :optimizations :none
                                    :pretty-print true
                                    :source-map true
-                                   :preamble ["resources/js/vendor/react-0.10.0/react_with_addons.min.js"
+                                   :preamble ["resources/js/vendor/react-0.10.0/react-with-addons.min.js"
                                               "resources/js/vendor/lz-string/lz-string-1.3.3-min.js"]
-                                   :externs ["resources/js/vendor/react-0.10.0/react_with_addons.js"
+                                   :externs ["resources/js/vendor/react-0.10.0/react-with-addons.js"
                                              "resources/js/vendor/lz-string/lz-string-1.3.3-min.js"]
                                    :closure-warnings {:externs-validation :off
-                                                      :non-standard-jsdoc :off}}}]}
+                                                      :non-standard-jsdoc :off}}}
+
+                       :prod
+                       {:source-paths ["src"]
+                        :compiler {:output-to "resources/main.min.js"
+                                   :optimizations :advanced
+                                   :pretty-print false
+                                   :preamble ["js/vendor/react-0.10.0/react-with-addons.min.js"
+                                              "js/vendor/lz-string/lz-string-1.3.3-min.js"]
+                                   :externs ["js/vendor/react-0.10.0/react-with-addons.js"
+                                             "js/vendor/lz-string/lz-string-1.3.3-min.js"]
+                                   :closure-warnings {:externs-validation :off
+                                                      :non-standard-jsdoc :off}}}}}
 )
