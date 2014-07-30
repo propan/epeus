6d07ed1b7334b25d157d5a581d351e52df62f9cc
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2014-07-30 22:26:31 +0300

    update the initial state (though I'm not sure now that the improved version is actually better)

--- src/epeus/state.cljs
+++ src/epeus/state.cljs
@@ -13,153 +13,153 @@
      :items {:uid      0
              :root?    true
              :title    "Mind Maps"
-             :x        398
-             :y        199
+             :x        369
+             :y        106
              :color    "#c0c0c0"
              :children {1 {:uid      1
                            :title    "communicate"
-                           :x        531
-                           :y        122
+                           :x        529
+                           :y        69
                            :color    "#67d7c4"
-                           :children {3 {:uid   3
-                                         :title "letter"
-                                         :x 655
-                                         :y 92
-                                         :color "#67d7c4"
+                           :children {3 {:uid      3
+                                         :title    "letter"
+                                         :x        642
+                                         :y        35
+                                         :color    "#67d7c4"
                                          :children {}}
                                       4 {:uid      4
                                          :title    "idea"
-                                         :x        656
-                                         :y        153
+                                         :x        642
+                                         :y        61
                                          :color    "#67d7c4"
                                          :children {}}
                                       5 {:uid      5
                                          :title    "speach"
-                                         :x        601
-                                         :y        182
+                                         :x        641
+                                         :y        86
                                          :color    "#67d7c4"
                                          :children {}}}}
                         7 {:uid      7
                            :title    "create"
-                           :x        562
-                           :y        257
+                           :x        528
+                           :y        222
                            :color    "#ebd95f"
-                           :children {8  {:uid      8
-                                          :title    "brain bloom"
-                                          :x        644
-                                          :y        241
-                                          :color    "#ebd95f"
-                                          :children {}}
-                                      9  {:uid      9
-                                          :title    "generate"
-                                          :x        631
-                                          :y        318
-                                          :color    "#ebd95f"
-                                          :children {}}
+                           :children {8 {:uid      8
+                                         :title    "brain bloom"
+                                         :x        587
+                                         :y        241
+                                         :color    "#ebd95f"
+                                         :children {}}
+                                      9 {:uid      9
+                                         :title    "generate"
+                                         :x        540
+                                         :y        267
+                                         :color    "#ebd95f"
+                                         :children {}}
                                       10 {:uid      10
                                           :title    "apply"
-                                          :x        469
-                                          :y        320
+                                          :x        444
+                                          :y        260
                                           :color    "#ebd95f"
                                           :children {}}}}
                         11 {:uid      11
                             :title    "think"
-                            :x        335
-                            :y        322
+                            :x        215
+                            :y        215
                             :color    "#e68782"
                             :children {12 {:uid      12
                                            :title    "problem"
-                                           :x        243
-                                           :y        293
+                                           :x        89
+                                           :y        191
                                            :color    "#e68782"
                                            :children {}}
                                        13 {:uid      13
                                            :title    "idea"
-                                           :x        250
-                                           :y        339
+                                           :x        121
+                                           :y        220
                                            :color    "#e68782"
                                            :children {}}
                                        14 {:uid      14
                                            :title    "vision"
-                                           :x        267
-                                           :y        390
+                                           :x        110
+                                           :y        256
                                            :color    "#e68782"
                                            :children {}}
                                        15 {:uid      15
                                            :title    "organize"
-                                           :x        390
-                                           :y        376
+                                           :x        250
+                                           :y        258
                                            :color    "#e68782"
                                            :children {}}}}
                         16 {:uid      16
                             :title    "manage"
-                            :x        287
-                            :y        221
+                            :x        205
+                            :y        135
                             :color    "#56e304"
-                            :children {17  {:uid      17
-                                            :title    "team"
-                                            :x        210
-                                            :y        165
-                                            :color    "#56e304"
-                                            :children {}}
-                                       18  {:uid      18
-                                            :title    "meetings"
-                                            :x        176
-                                            :y        208
-                                            :color    "#56e304"
-                                            :children {}}
+                            :children {17 {:uid      17
+                                           :title    "team"
+                                           :x        70
+                                           :y        107
+                                           :color    "#56e304"
+                                           :children {}}
+                                       18 {:uid      18
+                                           :title    "meetings"
+                                           :x        42
+                                           :y        132
+                                           :color    "#56e304"
+                                           :children {}}
                                        19 {:uid      19
                                            :title    "time"
-                                           :x        193
-                                           :y        279
+                                           :x        75
+                                           :y        154
                                            :color    "#56e304"
                                            :children {}}}}
                         20 {:uid      20
                             :title    "prepare"
-                            :x        235
-                            :y        128
+                            :x        304
+                            :y        69
                             :color    "#FF08E1"
                             :children {21 {:uid      21
                                            :title    "project"
-                                           :x        171
-                                           :y        51
+                                           :x        136
+                                           :y        36
                                            :color    "#FF08E1"
                                            :children {}}
                                        22 {:uid      22
                                            :title    "training"
-                                           :x        133
-                                           :y        113
+                                           :x        138
+                                           :y        66
                                            :color    "#FF08E1"
                                            :children {}}
                                        23 {:uid      23
                                            :title    "trip"
-                                           :x        124
-                                           :y        153
+                                           :x        167
+                                           :y        90
                                            :color    "#FF08E1"
                                            :children {}}}}
                         24 {:uid      24
                             :title    "improve"
-                            :x        357
-                            :y        132
+                            :x        539
+                            :y        151
                             :color    "#64AEFF"
                             :children {25 {:uid      25
                                            :title    "memory"
-                                           :x        244
-                                           :y        72
+                                           :x        629
+                                           :y        156
                                            :color    "#64AEFF"
                                            :children {}}
                                        26 {:uid      26
                                            :title    "productivity"
-                                           :x        344
-                                           :y        51
+                                           :x        629
+                                           :y        127
                                            :color    "#64AEFF"
                                            :children {}}
                                        27 {:uid      27
                                            :title    "understanding"
-                                           :x        467
-                                           :y        90
+                                           :x        631
+                                           :y        185
                                            :color    "#64AEFF"
-                                           :children {}}}}}}
+                                           :children {}}}}}}  
      }
 
     :tooltip
